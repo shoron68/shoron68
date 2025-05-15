2ee69be057fb37609d9ce233c12048c657ed76fc
shoron68 <mr.shoron.7514@gmail.com>
2025-05-15 22:18:24 +0600

    demo

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -21,28 +21,29 @@ const Loader = ({ onFinish }) => {
     <AnimatePresence>
       {isVisible && (
         <motion.div
-          className="fixed inset-0 max-w-[100%] bg-black flex items-center justify-center z-50"
-          initial={{ opacity: 1 }}
-          exit={{ opacity: 0, transition: { duration: 1 } }}
-        >
-          <motion.div
-            className="bg-[#80ed99] rounded-full"
-            initial={{ width: 64, height: 64 }}
-            animate={
-              isExpanded
-                ? {
-                    width: '3000px',
-                    height: '3000px',
-                    transition: { duration: 1, ease: 'easeInOut' },
-                  }
-                : {
-                    scale: [1, 1.5, 1],
-                    rotate: [0, 360],
-                    transition: { repeat: Infinity, duration: 1 },
-                  }
-            }
-          />
-        </motion.div>
+  className="fixed inset-0 bg-black flex items-center justify-center z-50 overflow-hidden"
+  initial={{ opacity: 1 }}
+  exit={{ opacity: 0, transition: { duration: 1 } }}
+>
+  <motion.div
+    className="bg-[#80ed99] rounded-full"
+    initial={{ width: 64, height: 64 }}
+    animate={
+      isExpanded
+        ? {
+            width: '3000px',
+            height: '3000px',
+            transition: { duration: 1, ease: 'easeInOut' },
+          }
+        : {
+            scale: [1, 1.5, 1],
+            rotate: [0, 360],
+            transition: { repeat: Infinity, duration: 1 },
+          }
+    }
+  />
+</motion.div>
+
       )}
     </AnimatePresence>
   );
